d36233257421b776c9f3c5462c53701ef3e9179a
Vamshi <vamshi@gmail.com>
2021-03-25 12:09:33 +0530

    bookingservice

--- src/services/Bookingservice.js
+++ src/services/Bookingservice.js
@@ -29,6 +29,12 @@ class Bookingservice {
     search(source,destination){
         return axios.get(BOOKINGS_API_BASE_URL+'/search/'+source+'/'+destination);
     }
+    userbooking(username){
+        return axios.get(BOOKINGS_API_BASE_URL+'/getusernamebookings/'+username);
+    }
+    deleteBooking(bookingId){
+        return axios.delete(BOOKINGS_API_BASE_URL+'/' +bookingId);
+    }
 }
 
 export default new Bookingservice()
